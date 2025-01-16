6679e01239e8e4976e5bf7fbd400c5a23798488d
dimeBrii <thiswasbrii@gmail.com>
2025-01-16 14:08:28 -0400

    ✨ Feat : adding luanch dependency

--- lib/presentation/widgets/courier/order_courier.dart
+++ lib/presentation/widgets/courier/order_courier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:GoDeli/features/order/domain/order.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OrderCourierCard extends StatelessWidget {
   final Courier courier;
@@ -8,21 +9,46 @@ class OrderCourierCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    void launchCaller(String phoneNumber) async {
+      final url = 'tel:+$phoneNumber';
+      try {
+        if (await canLaunchUrl(Uri.parse(url))) {
+          await launchUrl(Uri.parse(url));
+        } else {
+          print('Could not launch $url');
+          throw 'Could not launch $url';
+        }
+      } catch (e, stacktrace) {
+        print('Error launching $url: $e');
+        print(stacktrace);
+      }
+    }
+
     return Card(
       child: ListTile(
-        leading: Row(
-          mainAxisSize: MainAxisSize.min,
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.network(courier.courierImage, width: 70, height: 70),
+        ),
+        title: Text('Courier',
+            style:
+                TextStyle(color: colors.primary, fontWeight: FontWeight.w600)),
+        subtitle: Row(
           children: [
-            const Icon(Icons.motorcycle, color: Colors.red),
+            Text(courier.courierName, style: const TextStyle(fontSize: 16)),
             const SizedBox(width: 8),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(courier.courierImage, width: 50, height: 50),
+            GestureDetector(
+              onTap: () {
+                // launchCaller(courier.courierPhone);
+                print('Call functionality is currently disabled.');
+              },
+              child: Text('+${courier.courierPhone}',
+                  style: TextStyle(color: colors.primary)),
             ),
           ],
         ),
-        title: const Text('Courier'),
-        subtitle: Text(courier.courierName),
       ),
     );
   }
