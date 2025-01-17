bcef9b7dbfe93265aa06f105be2ec14df9bd9d2d
Alessandro Valsania <arvalsania.19@est.ucab.edu.ve>
2025-01-17 00:05:23 -0400

    🔧 Refactor: improve cart screen layout and styling

--- lib/presentation/screens/Cart/widgets/custom_cart_bundle.dart
+++ lib/presentation/screens/Cart/widgets/custom_cart_bundle.dart
@@ -13,6 +13,7 @@ class BundleWidget extends StatelessWidget {
     final cartBloc = context.watch<CartBloc>();
 
     final colors = Theme.of(context).colorScheme;
+    final textStyles = Theme.of(context).textTheme;
 
     return Slidable(
       endActionPane: ActionPane(
@@ -23,7 +24,7 @@ class BundleWidget extends StatelessWidget {
               onPressed: (context) {
                 cartBloc.add(RemoveBundle(bundle));
               },
-              backgroundColor: Colors.red,
+              backgroundColor: colors.primary,
               foregroundColor: Colors.white,
               icon: Icons.delete,
               label: 'Delete',
@@ -34,36 +35,23 @@ class BundleWidget extends StatelessWidget {
             )
           ]),
       child: Container(
-        padding: const EdgeInsets.only(left: 16, right: 8, top: 8, bottom: 8),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
+        padding: const EdgeInsets.all(8),
         child: Row(
           children: [
-            // Imagen del producto (puede ser un icono o una imagen de red)
             ClipRRect(
               borderRadius: BorderRadius.circular(8),
               child: bundle.bundle.imageUrl.isNotEmpty
                   ? Image.network(
                       bundle.bundle.imageUrl[0],
-                      width: 60,
-                      height: 60,
+                      width: 90,
+                      height: 120,
                       fit: BoxFit.cover,
                       errorBuilder: (context, error, stackTrace) {
                         return Container(
-                          width: 60,
-                          height: 60,
+                          width: 90,
+                          height: 120,
                           color: Colors.grey[200],
-                          child: Icon(
+                          child: const Icon(
                             Icons.broken_image,
                             color: Colors.grey,
                             size: 30,
@@ -72,7 +60,7 @@ class BundleWidget extends StatelessWidget {
                       },
                     )
                   : Icon(Icons.image_not_supported,
-                      size: 60, color: colors.onSurface.withOpacity(0.6)),
+                      size: 90, color: colors.onSurface.withOpacity(0.6)),
             ),
             const SizedBox(width: 8), // Espacio entre la imagen y el texto
             // Información del producto
@@ -80,79 +68,65 @@ class BundleWidget extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    bundle.bundle.name,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
+                  Text(bundle.bundle.name, style: textStyles.displayMedium),
                   const SizedBox(
                       width: 8), // Espacio entre la imagen y el texto
                   Text(
                     bundle.bundle.description,
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey[700],
-                    ),
-                    maxLines: 1,
+                    style: textStyles.bodyMedium
+                        ?.copyWith(color: Colors.grey[700]),
+                    maxLines: 2,
                     overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          '\$${bundle.bundle.price}',
+                          style: textStyles.displayMedium
+                        ),
+                      ),
+                      const SizedBox(width:16), // Espacio entre el precio y el control de cantidad
+                      // Control de cantidad
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        height: 35,
+                        child: Row(
+                          children: [
+                            QuantityButton(
+                                isMinus: true,
+                                product: bundle,
+                                icon: Icons.remove_rounded,
+                                color: colors.primary,
+                                onPressed: () {
+                                  cartBloc.add(RemoveQuantityBundle(bundle, 1));
+                                }),
+                            Text(
+                              '${bundle.quantity}', // Aquí muestra la cantidad actual
+                              style:textStyles.displaySmall,
+                            ),
+                            QuantityButton(
+                                isMinus: false,
+                                product: bundle,
+                                icon: Icons.add_rounded,
+                                color: colors.primary,
+                                onPressed: () {
+                                  cartBloc.add(AddQuantityBundle(bundle, 1));
+                                }),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
             ),
-            Column(
-              children: [
-                Text(
-                  '\$${bundle.bundle.price}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(
-                    width:
-                        16), // Espacio entre el precio y el control de cantidad
-                // Control de cantidad
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(32),
-                  ),
-                  height: 35,
-                  child: Row(
-                    children: [
-                      QuantityButton(
-                          isMinus: true,
-                          product: bundle,
-                          icon: Icons.remove_circle,
-                          color: colors.primary,
-                          onPressed: () {
-                            cartBloc.add(RemoveQuantityBundle(bundle, 1));
-                          }),
-                      Text(
-                        '${bundle.quantity}', // Aquí muestra la cantidad actual
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      QuantityButton(
-                          isMinus: false,
-                          product: bundle,
-                          icon: Icons.add_circle,
-                          color: colors.primary,
-                          onPressed: () {
-                            cartBloc.add(AddQuantityBundle(bundle, 1));
-                          }),
-                    ],
-                  ),
-                )
-              ],
-            )
+
             // Precio
-            ,
           ],
         ),
       ),
@@ -177,12 +151,14 @@ class QuantityButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     if (product.quantity == 1 && isMinus) {
       return IconButton(
           icon: Icon(icon, color: Colors.grey, size: 20), onPressed: null);
     }
 
     return IconButton(
-        icon: Icon(icon, color: Colors.red, size: 20), onPressed: onPressed);
+        icon: Icon(icon, color: colors.primary, size: 20), onPressed: onPressed);
   }
 }
